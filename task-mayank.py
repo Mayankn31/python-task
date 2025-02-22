abc6139522f250869c3812ef2e461594d58136bd
Mayank Nagpal <mayankn3173@gmail.com>
2025-02-22 07:41:17 +0000

    Updated task-mayank.py with username fix

--- task-mayank.py
+++ task-mayank.py
@@ -14,14 +14,16 @@ def get_top_output():
 @app.route('/htop')
 def htop():
     name = "Mayank Nagpal"  
-    username = os.getenv("USER", "Unknown User") 
+    system_username = os.getenv("USER", "Unknown User")  # Will show 'codespaces'
+    github_username = os.getenv("CODESPACES_USER", "mayankn31")  # Get actual GitHub username
     server_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S IST")
     top_output = get_top_output().replace("\n", "<br>")  
 
     return f"""
     <h1>System Info</h1>
     <p><strong>Name:</strong> {name}</p>
-    <p><strong>Username:</strong> {username}</p>
+    <p><strong>System Username:</strong> {system_username}</p>
+    <p><strong>GitHub Username:</strong> {github_username} </p>
     <p><strong>Server Time (IST):</strong> {server_time}</p>
     <pre>{top_output}</pre>
     """
